9d2564014f9df2f5451e279390a7c52ac63bf6c5
jeremiahlouis <131529321+jjboy55@users.noreply.github.com>
2023-11-08 09:15:07 +0100

    Initial commit

---
+++ lib/utilites/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoginAppTheme {
+  static ThemeData darkTheme = ThemeData();
+  static ThemeData lightTheme = ThemeData();
+  static LinearGradient splashScreenGradient = const LinearGradient(
+      colors: [Color(0xffFF9B63), Color.fromRGBO(255, 98, 31, 0.79)]);
+}
